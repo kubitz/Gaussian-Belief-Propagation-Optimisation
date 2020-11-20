a9aeaa872ff7c034c7f971b7517781cea1f132d6
Noe Guisset <nbg17@edge35.doc.ic.ac.uk>
2020-11-20 14:39:54 +0000

    fixed but slower

--- include/factor_graph.h
+++ include/factor_graph.h
@@ -27,13 +27,18 @@ public:
     #pragma omp parallel for
         for (auto &variable : variables_) {
             variable->update_belief();
+        #pragma omp critical
+        {  
             variable->send_messages();
         }
-    #pragma omp parallel for
+        }
+     #pragma omp parallel for
         for (auto &factor: factors_) {
             factor->update_factor();
+        #pragma omp critical
+        {  
             factor->send_messages();
-        }
+        }        }
         //for (auto &variable : variables_) {
         //}
         //for (auto &factor: factors_) {
