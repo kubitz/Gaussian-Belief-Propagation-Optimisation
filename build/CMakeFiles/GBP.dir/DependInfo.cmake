33a4c93687a08bb60dca3a14be69d9b8e4e29990
Louys Hong <louyshong1998@gmail.com>
2020-11-23 15:28:25 +0000

    add build files

---
+++ build/CMakeFiles/GBP.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/homes/tkh2017/COMPARCH-CW/src/main.cc" "/homes/tkh2017/COMPARCH-CW/build/CMakeFiles/GBP.dir/src/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../third_party"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/homes/tkh2017/COMPARCH-CW/build/CMakeFiles/libGBP.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GBP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GBP.dir/src/main.cc.o"
+  "GBP"
+  "GBP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GBP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
